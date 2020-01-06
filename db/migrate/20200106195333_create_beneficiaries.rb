f5f256a50379eb83871c1b2423a13689a4a4162b
fabian <bian251091@gmail.com>
2020-01-06 17:41:32 -0300

    second commit

---
+++ db/migrate/20200106195333_create_beneficiaries.rb
@@ -0,0 +1,15 @@
+class CreateBeneficiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :beneficiaries do |t|
+      t.integer :account_number
+      t.string :name
+      t.string :last_name
+      t.string :bank
+      t.integer :personal_id
+      t.integer :telephone_number
+      t.integer :mobile_pay
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+name = %w[Carlos Fabian Abraham Juan Pedro Milagros Genesis Gleisy Pablo Hugo Omar Robert]
+last_name = %w[Hernandez Fernandez Dios Eguez Bacigalupo Pedalino Arias Matos Pacheco Martinez Manelli]
+bank = %w[Banesco Mercantil Provincial]
+
+def create_beneficiary(name, last_name, bank)
+  Beneficiary.create(name: name.sample, last_name: last_name.sample,
+                     account_number: rand(100.1000), bank: bank.sample,
+                     personal_id: rand(555..666), telephone_number: rand(222..333),
+                     mobile_pay: rand(888..999))
+end
+10.times {create_beneficiary(name, last_name, bank)}
