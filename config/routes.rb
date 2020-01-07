f67cc024142f4aeb79fba97423bdcd74271e6cbe
fabian <bian251091@gmail.com>
2020-01-07 17:24:53 -0300

    skip endpoint registration user and act CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', :skip => [:registrations]
   root 'beneficiaries#index'
   resource :beneficiary, only: %i[index create update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
