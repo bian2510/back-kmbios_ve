e304f13864c11dfd4706cfad3bdbeb074b03ca0b
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-18 23:53:42 -0300

    add bancaribe

--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -4,6 +4,7 @@ class Beneficiary < ApplicationRecord
             :telephone_number, presence: true
   validates :bank, inclusion: { in: %w[banesco mercantil provincial
                                        venezuela tesoro exterior caroni
-                                       plaza fondo_comun 100%_banco bancrecer
-                                       banco_activo banplus bicentenario bnc] }
+                                       plaza fondo_comun 100%banco bancrecer
+                                       activo banplus bicentenario bnc 
+                                       bancaribe] }
 end
