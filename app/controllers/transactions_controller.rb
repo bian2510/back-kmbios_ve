20746afcf1c3fed0e0a4af90b8844700034efe25
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-22 15:58:01 -0300

    transaction controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,43 @@
+class  TransactionsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+      render json: transaction.where(user_id: current_user.id)
+    end
+  
+    def create
+      transaction = transaction.new(transaction_params)
+      transaction.user_id = current_user.id
+      transaction.in_progress
+      if transaction.save
+        render json: transaction, status: :created
+      else
+        render json: transaction.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      transaction = transaction.find(params[:id])
+      if transaction.update(transaction_params)
+        render json: transaction, status: :ok
+      else
+        render json: transaction.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      transaction = transaction.find(params[:id])
+      if transaction.destroy
+        render json: transaction, status: :ok
+      else
+        render json: transaction.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def transaction_params
+      params.require(:transaction).permit(:beneficiary_id, :rate, :currency,
+                                          :money_received, :money_sent)
+    end
+  end
+end  
