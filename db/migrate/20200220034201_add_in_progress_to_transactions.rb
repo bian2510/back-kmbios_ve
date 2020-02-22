06de6e50286fca47aaf09cb10dfd1812c25b98cc
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-22 15:58:54 -0300

    remove migration

--- db/migrate/20200220034201_add_in_progress_to_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddInProgressToTransactions < ActiveRecord::Migration[6.0]
-  def change
-    add_column :transactions, :in_progress, :boolean
-  end
-end
--- db/migrate/20200220034232_add_finished_to_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddFinishedToTransactions < ActiveRecord::Migration[6.0]
-  def change
-    add_column :transactions, :finished, :boolean
-  end
-end
