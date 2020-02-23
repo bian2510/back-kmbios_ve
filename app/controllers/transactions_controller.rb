fd7993a81a9d958da2121f656b122e3c4f1ab4ac
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-23 16:37:29 -0300

    transaction_controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,23 +1,28 @@
 class  TransactionsController < ApplicationController
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
     def index
-      render json: Transaction.where(user_id: current_user.id)
+      render json: Transaction.where(users: User.first)
     end
   
     def create
       transaction = Transaction.new(transaction_params)
-      transaction.user_id = current_user.id
-      transaction.in_progress
+      transaction.money_sent = transaction_params["money_received"] * transaction_params["rate"]
+      transaction.beneficiary_id = Beneficiary.find(params[:beneficiary_id]).id
+      transaction.users = [User.first, User.find(params[:user_id])]
+      transaction.in_progress = true
+      puts transaction
       if transaction.save
         render json: transaction, status: :created
       else
+        byebug
         render json: transaction.errors, status: :unprocessable_entity
       end
     end
   
     def update
       transaction = Transaction.find(params[:id])
-      if transaction.update(transaction_params)
+      transaction.finished = true
+      if transaction.save
         render json: transaction, status: :ok
       else
         render json: transaction.errors, status: :unprocessable_entity
@@ -34,10 +39,9 @@ class  TransactionsController < ApplicationController
     end
   
     private
-  
+
     def transaction_params
-      params.require(:transaction).permit(:beneficiary_id, :rate, :currency,
+      params.require(:transaction).permit(:rate, :currency,
                                           :money_received, :money_sent)
     end
-  end
 end  
