aeedfa8d1fe0bb4e7472ccee8047414a87e626d2
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-24 19:04:11 -0300

    add route to admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', :skip => [:registrations]
 
   mount_devise_token_auth_for 'Admin', at: 'admin_auth'
-  as :admin do
-    # Define routes for Admin within this block.
-  end
+  #as :admin do
+  #  # Define routes for Admin within this block.
+  #end
   root 'beneficiaries#index'
   resources :beneficiaries, only: %i[index create update destroy]
   resources :transactions, only: %i[index create update destroy]
