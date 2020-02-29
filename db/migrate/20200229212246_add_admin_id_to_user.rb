492c6f477728e40cfe215accc85988d79d82fec9
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-29 20:19:05 -0300

    add admin_id in models

---
+++ db/migrate/20200229212246_add_admin_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddAdminIdToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :users, :admin, foreign_key: true
+  end
+end
---
+++ db/migrate/20200229222650_add_admin_id_to_transactions.rb
@@ -0,0 +1,5 @@
+class AddAdminIdToTransactions < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :transactions, :admin, foreign_key: true
+  end
+end
