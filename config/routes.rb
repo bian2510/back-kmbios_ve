f7cb92ecfb923372fe7e21dade6b33a8e9f4b8e7
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-02-29 17:16:37 -0300

    change root in routes and change path of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:registrations]
-
+  mount_devise_token_auth_for 'User', at: 'user_auth'
   mount_devise_token_auth_for 'Admin', at: 'admin_auth'
   # as :admin do
-  #  # Define routes for Admin within this block.
+  #   # Define routes for Admin within this block.
   # end
-  root 'beneficiaries#index'
+  root 'main#index'
   resources :beneficiaries, only: %i[index create update destroy]
   resources :transactions, only: %i[index create update destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
