669005e7091bb405aed471b0f7dc2504e5866b44
Fabian Hernandez <itwarp_fpacheco@iseatz.com>
2020-04-28 15:20:44 -0300

    test mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  # Configuration for send mails in heroku
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'kmbios-ve-api-v1.herokuapp.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   default from: 'from@example.com'
   layout 'mailer'
 end
