3246e6a6f2ae4347f28c89a2985eef9048440b95
Hosward Villoria <hosward.villoria@rootstrap.com>
2020-05-02 20:08:18 -0300

    overrides the devise session controller

---
+++ app/controllers/overrides/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Overrides::SessionsController < DeviseTokenAuth::SessionsController
+
+  protected
+
+  def render_create_success
+    render json: {
+      data: resource_data(resource_json: @resource.token_validation_response)
+    }, scope: :current_admin
+  end
+end
